be5314ceb29e7afd6673025b0bee424db3a5c1c8
Mayank Kilhor <mayankkilhor@gmail.com>
2025-06-04 00:20:19 +0530

    Fixed Swagger UI for Auth Service

--- product-service/src/main/java/com/imspos/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/imspos/product_service/controller/ProductController.java
@@ -11,10 +11,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -23,6 +20,7 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @PostMapping("/createCategory")
     public ResponseEntity<?> createCategory(@Valid @RequestBody CreateCategoryRequest createCategoryRequest, @RequestHeader("x-user-id") String userId, @RequestHeader("x-username") String username ){
         try{
 
